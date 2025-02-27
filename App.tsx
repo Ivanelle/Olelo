6630d146c3cce2509fe8245b8e8373f8f9dc88ad
Ivanelle Hunt <ivanellep@gmail.com>
2025-02-27 13:02:57 -0800

    updated screens, error still showing that Navigation Container is nested in another

--- App.tsx
+++ App.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './app/components/HomeScreen'; 
+import HomeScreen from './app/components/HomeScreen';
 import HeartedWordsScreen from './app/components/Heart/HeartedWordsScreen';
 
-
+// Create a stack navigator
 const Stack = createStackNavigator();
 
-export default function App() {
+const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        {/* Home Screen */}
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{ title: 'Kamailio' }} // Custom title for the home screen
         />
+        {/* Hearted Words Screen */}
         <Stack.Screen
           name="HeartedWords"
           component={HeartedWordsScreen}
-          options={{ title: 'My Words' }} // Custom title for the hearted words screen
+          options={{ title: 'Hearted Words' }} // Custom title for the hearted words screen
         />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
+
+export default App;
